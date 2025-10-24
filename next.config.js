6e8261f5a303518ee5b7693b49c211621eeb09e6
mijelblack677-ctrl <mijelblack677@gmail.com>
2025-10-24 14:34:42 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  // Remove experimental.appDir since it's enabled by default now
 }
 
 module.exports = nextConfig
