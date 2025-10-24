86505c5682c48acb70df1e24e6b9a63a85704500
mijelblack677-ctrl <mijelblack677@gmail.com>
2025-10-24 14:54:13 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { SearchProvider } from '../contexts/SearchContext';
+import { MusicProvider } from '../contexts/MusicContext';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <SearchProvider>
-      <Component {...pageProps} />
-    </SearchProvider>
+    <MusicProvider>
+      <SearchProvider>
+        <Component {...pageProps} />
+      </SearchProvider>
+    </MusicProvider>
   );
 }
